4b0dde1b3e34714fa3bc269a2260ab3a2b628532
Dennis Kortekaas <denniskortekaas@MBP-van-Dennis.home>
2022-07-07 21:36:22 +0200

    Engine locking

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Starter <a href='https://nextjs.org'>Next.js!</a>
+          <a href='https://nextjs.org'>Next.js</a> Starter!
         </h1>
 
         <p className={styles.description}>
